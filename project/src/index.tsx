a4cf9dc722a240523e9d479b2ed5c30e897e6e88
irealife <irealife@rambler.ru>
2021-10-02 22:15:52 +0300

    Разбивка на компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const InfoPromoFilm = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  RELEASE_DATE: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={InfoPromoFilm.TITLE}
+      genre={InfoPromoFilm.GENRE}
+      releaseDate={InfoPromoFilm.RELEASE_DATE}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
